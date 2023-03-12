9557611bad4c4c69fa7a67ffd40ab021e41b94ac
Shivamt2107 <shivam.gote2107@gmail.com>
2023-03-12 13:57:24 +0530

    add file

--- big_basket/src/All-Routes/PrivateRoute.js
+++ big_basket/src/All-Routes/PrivateRoute.js
@@ -7,6 +7,8 @@ Dont make any changes to this file
 function PrivateRoute({children}) {
   const authState = useSelector((state)=>state.auth)
 
+
+  
   if(!authState.auth_sucess){
     return <Navigate to={'/login'} />
   }
